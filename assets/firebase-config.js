9e32efc65948d465e00e3ed55babba4ce93c934b
WISEinc-pharmacy <wiseinc.pharmacy@gmail.com>
2026-07-01 17:46:03 +0900

    Firebase Web設定を反映

--- assets/firebase-config.js
+++ assets/firebase-config.js
@@ -1,8 +1,8 @@
 window.RESKILLING_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyB10EkMLI2DX3hFs-9aF55wASSIrLjC0M8",
+  authDomain: "pharmacist-reskilling-members.firebaseapp.com",
+  projectId: "pharmacist-reskilling-members",
+  storageBucket: "pharmacist-reskilling-members.firebasestorage.app",
+  messagingSenderId: "896005823878",
+  appId: "1:896005823878:web:0e54c9ac03c40b803e2007"
 };
